f5ebd2af86f1c49059af15bcebecc6d45e569075
harry-shimokawa <harry.s2027@gmail.com>
2025-03-10 02:58:10 +1000

    CH1: Set up & CH2: Data Analysis

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-with open(path_to_file) as f:
-    file_contents = f.read()
-    
+from stats import get_num_words, get_chars_dict
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"{num_words} words found in the document")
+    print("")
+    print(chars_dict)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+
+main()
