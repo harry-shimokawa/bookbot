063ae4f790ecdbacb7ba31304d2667733eef46af
harry-shimokawa <harry.s2027@gmail.com>
2025-03-16 10:12:33 +1000

    CH3: Report L2: Arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -5,8 +6,12 @@ from stats import (
 )
 
 
-def main():
-    book_path = "books/frankenstein.txt"
+def main():    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -33,4 +38,4 @@ def print_report(book_path, num_words, chars_sorted_list):
     print("============= END ===============")
 
 
-main()
+main ()
